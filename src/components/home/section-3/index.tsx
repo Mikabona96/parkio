f596fdfa09200493831e43239d5bd45008423f35
Dmytro Holub <dmitriygolubapk@gmail.com>
2024-04-02 20:22:23 +0300

    Components: home section-3 Slider appearence/vanish animation

--- src/components/home/section-3/index.tsx
+++ src/components/home/section-3/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button } from '@/elements';
+import { cn } from '@/tools/utils/cn';
 import clsx from 'clsx';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
@@ -7,127 +8,140 @@ import React, { useState } from 'react';
 import { setInterval } from 'timers/promises';
 
 export const ThirdSection = () => {
-  const [activeSlide, setActiveSlide] = useState(0);
-  const t = useTranslations('Home');
+	const [activeSlide, setActiveSlide] = useState(0);
+	const t = useTranslations('Home');
 
-  const slidesDescription = [
-    t('s-3-slide-1'),
-    t('s-3-slide-2'),
-    t('s-3-slide-3'),
-    t('s-3-slide-4'),
-  ];
+	const slidesDescription = [
+		t('s-3-slide-1'),
+		t('s-3-slide-2'),
+		t('s-3-slide-3'),
+		t('s-3-slide-4'),
+	];
 
-  return (
-    <div className="flex flex-col items-center py-[120px]">
-      <h3 className="text-xl font-normal text-gray-700">{t('s-3-title')}</h3>
-      <h4 className="text-[32px] font-bold text-gray-900">
-        {t('s-3-subtitle')}
-      </h4>
-      <div className="flex w-[400px] justify-between">
-        <button
-          className="cursor-pointer rounded border bg-gradient-1 px-2 py-2"
-          onClick={() => {
-            if (activeSlide === 3) {
-              setActiveSlide(0);
-            } else {
-              setActiveSlide(activeSlide + 1);
-            }
-          }}
-        >
-          Next Step
-        </button>
-        <button
-          className="cursor-pointer rounded border bg-gradient-1 px-2 py-2"
-          onClick={() => {
-            if (activeSlide === 0) {
-              setActiveSlide(3);
-            } else {
-              setActiveSlide(activeSlide - 1);
-            }
-          }}
-        >
-          Prev Step
-        </button>
-      </div>
-      <div className="mt-12 flex h-[560px] items-center gap-16 overflow-hidden">
-        <div className="flex h-[560px] w-[534px] justify-center bg-[url('/gradient_circle_background.png')]">
-          {activeSlide === 0 && (
-            <Image
-              className={
-                'animated-slide h-[600px] w-[308px] animate-appearence'
-              }
-              width={534}
-              height={602}
-              src={`/slide-1.png`}
-              alt={`slide-1.png`}
-            />
-          )}
-          {activeSlide === 1 && (
-            <Image
-              className={
-                'animated-slide h-[600px] w-[308px] animate-appearence'
-              }
-              width={534}
-              height={602}
-              src={`/slide-2.png`}
-              alt={`slide-2.png`}
-            />
-          )}
-          {activeSlide === 2 && (
-            <Image
-              className={
-                'animated-slide h-[600px] w-[308px] animate-appearence'
-              }
-              width={534}
-              height={602}
-              src={`/slide-3.png`}
-              alt={`slide-3.png`}
-            />
-          )}
-          {activeSlide === 3 && (
-            <Image
-              className={
-                'animated-slide h-[600px] w-[308px] animate-appearence'
-              }
-              width={534}
-              height={602}
-              src={`/slide-4.png`}
-              alt={`slide-4.png`}
-            />
-          )}
-        </div>
-        <div
-          className={clsx(
-            'relative flex h-[348px] w-[388px] flex-col justify-between border-l-2 border-l-gray-200 text-left transition duration-300 ease-in-out before:absolute before:-left-[12px] before:top-0 before:block before:h-[40px] before:w-[20px] before:transition before:duration-300 before:ease-in-out before:content-[url("/slider.png")]',
-            {
-              "before:absolute before:-left-[12px] before:top-0 before:block before:h-[40px] before:w-[20px] before:translate-y-[87px] before:content-[url('/slider.png')]":
-                activeSlide === 1,
-              "before:absolute before:-left-[12px] before:top-0 before:block before:h-[40px] before:w-[20px] before:translate-y-[174px] before:content-[url('/slider.png')]":
-                activeSlide === 2,
-              "before:absolute before:-left-[12px] before:top-0 before:block before:h-[40px] before:w-[20px] before:translate-y-[261px] before:content-[url('/slider.png')]":
-                activeSlide === 3,
-            },
-          )}
-        >
-          {slidesDescription.map((slideDescr, idx) => {
-            return (
-              <div
-                onClick={() => setActiveSlide(idx)}
-                key={idx}
-                className={clsx(
-                  'relative flex h-[87px] cursor-pointer items-center text-xl font-normal text-gray-700',
-                  {
-                    'text-gray-300': idx !== activeSlide,
-                  },
-                )}
-              >
-                <p className="ml-6">{slideDescr}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <Button className="mt-12">Learn more</Button>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center py-[120px]">
+			<h3 className="text-xl font-normal text-gray-700">{t('s-3-title')}</h3>
+			<h4 className="text-[32px] font-bold text-gray-900">
+				{t('s-3-subtitle')}
+			</h4>
+			<div className="flex w-[400px] justify-between">
+				<button
+					className="cursor-pointer rounded border bg-gradient-1 px-2 py-2"
+					onClick={() => {
+						if (activeSlide === 3) {
+							setActiveSlide(0);
+						} else {
+							setActiveSlide(activeSlide + 1);
+						}
+					}}
+				>
+					Next Step
+				</button>
+				<button
+					className="cursor-pointer rounded border bg-gradient-1 px-2 py-2"
+					onClick={() => {
+						if (activeSlide === 0) {
+							setActiveSlide(3);
+						} else {
+							setActiveSlide(activeSlide - 1);
+						}
+					}}
+				>
+					Prev Step
+				</button>
+			</div>
+			<div className="mt-12 flex h-[560px] items-center gap-16 overflow-hidden">
+				<div className="relative flex h-[560px] w-[534px] justify-center bg-[url('/gradient_circle_background.png')]">
+					<Image
+						className={cn(
+							'absolute left-[50%] h-[600px] w-[308px] -translate-x-[50%] animate-appearence',
+							{
+								'animate-appearence': activeSlide === 0,
+								'z-[22]': activeSlide === 0,
+								'z-[11]': activeSlide !== 0,
+								'animate-vanish': activeSlide === 1 || activeSlide === 3,
+							},
+						)}
+						width={534}
+						height={602}
+						src={`/slide-1.png`}
+						alt={`slide-1.png`}
+					/>
+					<Image
+						className={cn(
+							'absolute left-[50%] h-[600px] w-[308px] -translate-x-[50%] animate-appearence',
+							{
+								'animate-appearence': activeSlide === 1,
+								'z-[22]': activeSlide === 1,
+								'z-[11]': activeSlide !== 1,
+								'animate-vanish': activeSlide === 2 || activeSlide === 0,
+							},
+						)}
+						width={534}
+						height={602}
+						src={`/slide-2.png`}
+						alt={`slide-2.png`}
+					/>
+					<Image
+						className={cn(
+							'absolute left-[50%] h-[600px] w-[308px] -translate-x-[50%] animate-appearence',
+							{
+								'animate-appearence': activeSlide === 2,
+								'z-[22]': activeSlide === 2,
+								'z-[11]': activeSlide !== 2,
+								'animate-vanish': activeSlide === 3 || activeSlide === 1,
+							},
+						)}
+						width={534}
+						height={602}
+						src={`/slide-3.png`}
+						alt={`slide-3.png`}
+					/>
+					<Image
+						className={cn(
+							'absolute left-[50%] h-[600px] w-[308px] -translate-x-[50%] animate-appearence',
+							{
+								'animate-appearence': activeSlide === 3,
+								'z-[22]': activeSlide === 3,
+								'z-[11]': activeSlide !== 3,
+								'animate-vanish': activeSlide === 0 || activeSlide === 2,
+							},
+						)}
+						width={534}
+						height={602}
+						src={`/slide-4.png`}
+						alt={`slide-4.png`}
+					/>
+				</div>
+				<div
+					className={clsx(
+						'relative flex h-[348px] w-[388px] flex-col justify-between border-l-2 border-l-gray-200 text-left duration-300 ease-in-out before:absolute before:-left-[12px] before:top-0 before:block before:h-[40px] before:w-[20px] before:transition before:duration-[.7s] before:ease-in-out before:content-[url("/slider.png")]',
+						{
+							'before:translate-y-[87px]': activeSlide === 1,
+							'before:translate-y-[174px]': activeSlide === 2,
+							'before:translate-y-[261px]': activeSlide === 3,
+						},
+					)}
+				>
+					{slidesDescription.map((slideDescr, idx) => {
+						return (
+							<div
+								onClick={() => setActiveSlide(idx)}
+								key={idx}
+								className={clsx(
+									'relative flex h-[87px] cursor-pointer items-center text-xl font-normal text-gray-700',
+									{
+										'text-gray-300': idx !== activeSlide,
+									},
+								)}
+							>
+								<p className="ml-6">{slideDescr}</p>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+			<Button className="mt-12">Learn more</Button>
+		</div>
+	);
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,72 +1,96 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/elements/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      gray: {
-        '200': '#EAECF0',
-        '300': '#D0D5DD',
-        '500': '#667085',
-        '700': '#344054',
-        '900': '#101828',
-      },
-      gradient: {
-        '1': '#FFD177',
-        '2': '#FFA1A3',
-        '3': '#D18FE9',
-      },
-    },
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      boxShadow: {
-        select: '0px 8px 16px 0px rgba(27, 33, 44, 0.10)',
-      },
-      animation: {
-        appearence: 'appearence .3s ease-in-out',
-      },
-      keyframes: {
-        appearence: {
-          '0%': {
-            opacity: '0.1',
-          },
-          '20%': {
-            opacity: '0.2',
-          },
-          '30%': {
-            opacity: '0.3',
-          },
-          '50%': {
-            opacity: '0.5',
-          },
-          '70%': {
-            opacity: '0.7',
-          },
-          '80%': {
-            opacity: '0.8',
-          },
-          '100%': {
-            opacity: '1',
-          },
-        },
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/elements/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		colors: {
+			gray: {
+				'200': '#EAECF0',
+				'300': '#D0D5DD',
+				'500': '#667085',
+				'700': '#344054',
+				'900': '#101828',
+			},
+			gradient: {
+				'1': '#FFD177',
+				'2': '#FFA1A3',
+				'3': '#D18FE9',
+			},
+		},
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			boxShadow: {
+				select: '0px 8px 16px 0px rgba(27, 33, 44, 0.10)',
+			},
+			animation: {
+				appearence: 'appearence .3s ease-in-out',
+				vanish: 'vanish .4s ease-out',
+			},
+			keyframes: {
+				appearence: {
+					'0%': {
+						opacity: '0.1',
+					},
+					'20%': {
+						opacity: '0.2',
+					},
+					'30%': {
+						opacity: '0.3',
+					},
+					'50%': {
+						opacity: '0.5',
+					},
+					'70%': {
+						opacity: '0.7',
+					},
+					'80%': {
+						opacity: '0.8',
+					},
+					'100%': {
+						opacity: '1',
+					},
+				},
+				vanish: {
+					'0%': {
+						opacity: '1',
+					},
+					'20%': {
+						opacity: '0.8',
+					},
+					'30%': {
+						opacity: '0.7',
+					},
+					'50%': {
+						opacity: '0.5',
+					},
+					'70%': {
+						opacity: '0.3',
+					},
+					'80%': {
+						opacity: '0.2',
+					},
+					'100%': {
+						opacity: '0',
+					},
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
