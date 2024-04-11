54ba0ea774799f5b56e7fb0554ab16eb49103af1
Dmytro Holub <dmitriygolubapk@gmail.com>
2024-04-11 16:54:46 +0300

    Page: reset-password

---
+++ src/app/[locale]/reset-password/page.tsx
@@ -0,0 +1,12 @@
+import { FirstSection } from '@/components/reset-password';
+import React from 'react';
+
+const ResetPassword = () => {
+	return (
+		<main>
+			<FirstSection />
+		</main>
+	);
+};
+
+export default ResetPassword;
--- src/components/reset-password/index.tsx
+++ src/components/reset-password/index.tsx
@@ -3,14 +3,23 @@ import { useTranslations } from 'next-intl';
 import React from 'react';
 
 export const FirstSection = () => {
-	const t = useTranslations('Forgot-password');
+	const t = useTranslations('Reset-password');
 	return (
 		<section className="flex items-center justify-center py-[120px]">
 			<div className="flex w-full max-w-[354px] flex-col text-center">
 				<h1 className="text-[32px] font-bold text-gray-900">{t('title')}</h1>
 				<h3 className="mt-4">{t('subtitle')}</h3>
 				<form action="" className="mt-8 flex flex-col">
-					<input placeholder="Email" type="email" className="form-input h-11" />
+					<input
+						placeholder={t('password')}
+						type="password"
+						className="form-input mt-4 h-11"
+					/>
+					<input
+						placeholder={t('confirm-password')}
+						type="password"
+						className="form-input mt-4 h-11"
+					/>
 					<Button className="mt-4">{t('button')}</Button>
 				</form>
 				<div className="mt-4 h-[1px] w-full bg-gray-300"></div>
